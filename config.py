a1e5ef7b10389fed623c9f0b3eba9e774089b204
Brent Klein <bklein@xometry.com>
2019-12-20 13:38:17 -0500

    Added praw and set up env vars

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
-USER_NAME = os.environ['USER_NAME']
-PASSWORD = os.environ['PASSWORD']
+username = os.environ['praw_username']
+password = os.environ['praw_password']
+client_id = os.environ['praw_client_id']
+client_secret = os.environ['praw_client_secret']
