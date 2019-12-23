d09304a2435c2017deb24ab29d10f6ddc83d9ad5
Brent Klein <bklein@xometry.com>
2019-12-23 17:11:26 -0500

    Added suitability tests to avoid posts/subs where bots aren't allowed

--- app/bot.py
+++ app/bot.py
@@ -1,4 +1,4 @@
-import time
+import json
 
 from config import Site
 from praw.exceptions import APIException
@@ -45,73 +45,77 @@ compliments = [
     "an inspiration"
 ]
 
+with open('subreddits.json') as json_file:
+    subreddits = json.load(json_file)
+
+restricted_subreddits = subreddits['restricted']
+
 
 def reply_to_comment(c, m):
     c.reply(m)
 
 
+def thread_is_suitable(c):
+    return \
+        c.submission.subreddit.display_name not in restricted_subreddits and \
+        not c.submission.subreddit.over18 and \
+        'serious' not in c.submission.title.lower() and \
+        c.author is not None and \
+        c.author.name != self_name
+
+
+# get a stream of new comments
 print("monitoring new comments...")
-while True:
-    # get a stream of new comments
-    comments_checked = 0
-    compliments_found = 0
-    replies_made = 0
+comments_checked = 0
+compliments_found = 0
+replies_made = 0
 
-    for comment in sub.stream.comments(skip_existing=True):
-        comments_checked += 1
-        if compliments_found > 0 and compliments_found % 10 == 0 or comments_checked % 100 == 0:
-            print(f"comments checked: {comments_checked}; compliments found: {compliments_found}")
+for comment in sub.stream.comments(skip_existing=True):
+    comments_checked += 1
+    if compliments_found > 0 and compliments_found % 10 == 0 or comments_checked % 100 == 0:
+        print(f"comments checked: {comments_checked}; compliments found: {compliments_found}")
 
-        # Avoid the rough side of town
-        if comment.subreddit.over18:
-            continue
+    # Check for thread suitability
+    if not thread_is_suitable(comment):
+        continue
 
-        if comment.author is None or comment.author.name == self_name:
-            # Don't compliment self, would get caught in a loop, as well as being tacky
-            # Skip comments without an author
-            continue
+    compliment_used = None
+    message = None
 
-        compliment_used = None
-        message = None
-
-        # check each comment for any combination of objects, [adverbs], and compliments
-        for subject in subjects:
-            for compliment in compliments:
-                if f"{subject} {compliment}" in comment.body.lower():
-                    compliment_used = compliment
+    # check each comment for any combination of objects, [adverbs], and compliments
+    for subject in subjects:
+        for compliment in compliments:
+            if f"{subject} {compliment}" in comment.body.lower():
+                compliment_used = compliment
+                break
+            for adverb in adverbs:
+                if f"{subject} {adverb} {compliment}" in comment.body.lower():
+                    compliment_used = f"{adverb} {compliment}"
                     break
-                for adverb in adverbs:
-                    if f"{subject} {adverb} {compliment}" in comment.body.lower():
-                        compliment_used = f"{adverb} {compliment}"
-                        break
-                if compliment_used is not None:
-                    break
+            if compliment_used is not None:
+                break
 
-        if compliment_used is not None:
-            # check each comment with a compliment for a response from self
-            # if no response, respond
-            if self_name not in [reply.author.name for reply in list(comment.replies) if reply.author is not None]:
-                compliments_found += 1
-                print(f"posting reply in {comment.subreddit.display_name}")
-                if comment.parent().author.name == self_name:
-                    # We're being complimented!
-                    message = f"Awww, thanks u/{comment.author.name}. =)"
-                elif compliment_used == 'great':
-                    # r/beetlejuicing incoming
-                    message = f"Check my username, u/{comment.author.name}. =)"
-                else:
-                    message = f"*You're* {compliment_used}, u/{comment.author.name}!"
+    if compliment_used is not None:
+        # check each comment with a compliment for a response from self
+        # if no response, respond
+        if self_name not in [reply.author.name for reply in list(comment.replies) if reply.author is not None]:
+            compliments_found += 1
+            print(f"posting reply in {comment.subreddit.display_name}")
+            if comment.parent().author.name == self_name:
+                # We're being complimented!
+                message = f"Awww, thanks u/{comment.author.name}. =)"
+            elif compliment_used == 'great':
+                # r/beetlejuicing incoming
+                message = f"Check my username, u/{comment.author.name}. =)"
+            else:
+                message = f"*You're* {compliment_used}, u/{comment.author.name}!"
 
-                try:
-                    reply_to_comment(comment, message)
-                    replies_made += 1
-                except APIException as e:
-                    # We expect rate limit issues since the account is new and we're processing a lot of comments
-                    # quickly. Ignore those exceptions, but raise any others.
-                    print(f'Encountered API exception: {str(e)}')
-                    if "RATELIMIT" not in str(e):
-                        raise e
-
-    # Refresh the stream every fifteen minutes if no new comments are yielded
-    print("sleeping...")
-    time.sleep(900)
+            try:
+                reply_to_comment(comment, message)
+                replies_made += 1
+            except APIException as e:
+                # We expect rate limit issues since the account is new and we're processing a lot of comments
+                # quickly. Log those exceptions, but also raise any others.
+                print(f'Encountered API exception: {str(e)}')
+                if "RATELIMIT" not in str(e):
+                    raise e
